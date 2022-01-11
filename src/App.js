14d6560a2b8ebe3bae7983f91dac88631745ee34
seonhwa <87746574+kmseonhwa@users.noreply.github.com>
2022-01-11 15:44:45 +0900

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Link, Outlet } from "react-router-dom";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav
+                style={{
+                    borderBottom: "solid 1px",
+                    paddingBottom: "1rem"
+                }}
+            >
+                <Link to="/invoices">Invoices</Link> |{" "}
+                <Link to="/expenses">Expenses</Link>
+            </nav>
+            <Outlet />
+        </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route path="expenses" element={<Expenses />} />
+                <Route path="invoices" element={<Invoices />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>,
+    rootElement
 );
