26b97801047ba4b8bf94d54b43e5f416b5b4569b
seonhwa <87746574+kmseonhwa@users.noreply.github.com>
2022-01-11 17:26:50 +0900

    Search Params

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,28 +1,47 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../data";
 
 const Invoices = () => {
     const invoices = getInvoices();
+    const [searchParams, setSearchParams] = useSearchParams();
+    // searchParams.set vs setSearchParams
 
     return (
         <div style={{ padding: "1rem 0" }}>
+            <input
+                value={searchParams.get("filter") || ""}
+                onChange={(e) => {
+                    const filter = e.target.value;
+                    if (filter) {
+                        setSearchParams({ filter });
+                    } else {
+                        setSearchParams({});
+                    }
+                }}
+            />
             <ul>
-                {invoices.map((invoice) => (
-                    <li>
-                        <NavLink
-                            style={({ isActive }) => {
-                                return {
-                                    color: isActive ? "red" : ""
-                                };
-                            }}
-                            to={`/invoices/${invoice.number}`}
-                            key={invoice.number}
-                            // className={({ isActive }) => isActive ? "red" : "blue"}
-                        >
-                            {invoice.name}
-                        </NavLink>
-                    </li>
-                ))}
+                {invoices
+                    .filter((invoice) => {
+                        const filter = searchParams.get("filter");
+                        if (!filter) return;
+                        const name = invoice.name.toLowerCase();
+                        return name.startsWith(filter.toLowerCase());
+                    })
+                    .map((invoice) => (
+                        <li key={invoice.number}>
+                            <NavLink
+                                style={({ isActive }) => {
+                                    return {
+                                        color: isActive ? "red" : ""
+                                    };
+                                }}
+                                to={`/invoices/${invoice.number}`}
+                                // className={({ isActive }) => isActive ? "red" : "blue"}
+                            >
+                                {invoice.name}
+                            </NavLink>
+                        </li>
+                    ))}
             </ul>
             <Outlet />
         </div>
